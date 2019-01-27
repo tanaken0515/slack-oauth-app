af21c6a119675b1d24ff899a0ae7ef73f3824f54
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2019-01-27 10:32:51 +0900

    before_actionで基本的にはログイン必須にする ただし、SessionsControllerはskip_before_actionしとく

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  before_action :login_required
+
+  private
+
+  def session_effective?
+    begin
+      # 毎回APIリクエストを投げるのは多すぎる気がする。Rails.cacheに入れる？
+      Slack::Auth.new(oauth_response[:access_token]).auth_test
+    rescue Slack::Web::Api::Error
+      # todo: tokenを無効化したほうがいいかも auth.revoke API
+      reset_session
+      return false
+    end
+    true
+  end
+
+  def login_required
+    redirect_to login_path unless session_effective?
+  end
 end
